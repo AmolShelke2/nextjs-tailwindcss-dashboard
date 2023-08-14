0a89e0145fe06dbe5ab5f7c81425700bec6d3384
Amol <shelkeamol897@gmail.com>
2023-08-14 20:56:47 +0530

    updated

--- src/dashboard/sidebar/SidebarItems.tsx
+++ src/dashboard/sidebar/SidebarItems.tsx
@@ -5,8 +5,8 @@ import { useRouter } from "next/router";
 const style = {
   title: "font-normal mx-4 text-sm",
   active:
-    "border-r-4 border-blue-500 border-r-4 border-blue-500 bg-[#5D87FF] text-white lg:hover:bg-[#5D87FF] lg:hover:text-white",
-  link: "duration-200 flex font-thin items-center justify-start my-2 p-4 transition-colors text-gray-500 uppercase rounded-xl lg:hover:bg-[#ECF2FF] lg:hover:text-blue-500 mx-4",
+    "border-r-4 border-blue-500 border-r-4 border-blue-500 bg-[#5D87FF] lg:hover:bg-[#5D87FF] text-white",
+  link: "duration-200 flex font-thin items-center justify-start my-2 p-4 transition-colors text-gray-500 uppercase rounded-xl lg:hover:bg-[#ECF2FF] mx-4",
 };
 
 export function SidebarItems() {

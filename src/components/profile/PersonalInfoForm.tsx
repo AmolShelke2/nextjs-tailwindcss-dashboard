696362a37f55755744c66305ba7eee3205307d14
Amol <shelkeamol897@gmail.com>
2023-08-18 23:41:23 +0530

    Form optimized

--- src/components/profile/PersonalInfoForm.tsx
+++ src/components/profile/PersonalInfoForm.tsx
@@ -8,11 +8,7 @@ interface FormData {
   gender: string;
 }
 
-interface PersonalInfoFormProps {
-  onNext: () => void;
-}
-
-const PersonalInfoForm: React.FC<PersonalInfoFormProps> = ({ onNext }) => {
+const PersonalInfoForm: React.FC = () => {
   const [formData, setFormData] = useState<FormData>({
     fullName: "",
     email: "",
@@ -35,81 +31,98 @@ const PersonalInfoForm: React.FC<PersonalInfoFormProps> = ({ onNext }) => {
   };
 
   return (
-    <div className="h-full w-full py-10 relative">
-      <form
-        className="flex justify-between items-start px-8 h-full"
-        onSubmit={handleSubmit}>
-        <div className="flex flex-col w-[350px]">
-          <label>Full Name</label>
-          <input
-            type="text"
-            name="fullName"
-            placeholder="Enter full name"
-            required
-            className="w-full mb-8 mt-4 border-gray-400 py-2 border focus:outline-none rounded-sm px-2"
-            onChange={handleInputChange}
-          />
-          <label>Email Address</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter email address"
-            className="w-full mb-8 mt-4 border-gray-400 py-2 border focus:outline-none rounded-sm px-2 capitalize"
-            required
-            onChange={handleInputChange}
-          />
+    <div className="flex flex-col gap-9">
+      {/* <!-- Contact Form --> */}
+      <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="border-b border-stroke py-4 px-6 dark:border-strokedark">
+          <h3 className="font-medium text-black">Contact Form</h3>
         </div>
+        <form action="#">
+          <div className="p-6">
+            <div className="mb-4 flex flex-col gap-6 xl:flex-row">
+              <div className="w-full xl:w-1/2">
+                <label className="mb-2 block text-black">First name</label>
+                <input
+                  type="text"
+                  placeholder="Enter your first name"
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                />
+              </div>
 
-        <div className="flex flex-col w-[350px]">
-          <label>Phone Number</label>
-          <input
-            type="number"
-            name="phoneNumber"
-            placeholder="Mobile Number"
-            className="w-full mb-8 mt-4 border-gray-400 py-2 border focus:outline-none rounded-sm px-2"
-            required
-            onChange={handleInputChange}
-          />
-          <label>DOB</label>
-          <input
-            type="date"
-            name="dob"
-            className="uppercase w-full mb-8 mt-4 border-gray-400 py-2 border focus:outline-none rounded-sm px-2"
-            required
-            onChange={handleInputChange}
-          />
-        </div>
+              <div className="w-full xl:w-1/2">
+                <label className="mb-2 block text-black">Last name</label>
+                <input
+                  type="text"
+                  placeholder="Enter your last name"
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                />
+              </div>
+            </div>
 
-        <div className="flex flex-col w-[350px]">
-          <p className="mb-3">Gender</p>
-          <div className="flex flex-row items-center text-center">
-            <input
-              type="radio"
-              id="male"
-              name="gender"
-              value="male"
-              className="cursor-pointer mr-2"
-              onChange={handleInputChange}
-            />
-            <label className="mr-5">Male</label>
-            <input
-              type="radio"
-              id="female"
-              name="gender"
-              value="female"
-              className="cursor-pointer mr-2"
-              onChange={handleInputChange}
-            />
-            <label>Female</label>
+            <div className="mb-4">
+              <label className="mb-2 block text-black">
+                Email <span className="text-meta-1">*</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Enter your email address"
+                className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="mb-2 block text-black">Subject</label>
+              <input
+                type="text"
+                placeholder="Select subject"
+                className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="mb-2 block text-black">Subject</label>
+              <div className="relative z-20 bg-transparent dark:bg-form-input">
+                <select className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary">
+                  <option value="">Type your subject</option>
+                  <option value="">USA</option>
+                  <option value="">UK</option>
+                  <option value="">Canada</option>
+                </select>
+                <span className="absolute top-1/2 right-4 z-30 -translate-y-1/2">
+                  <svg
+                    className="fill-current"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <g opacity="0.8">
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                        fill=""></path>
+                    </g>
+                  </svg>
+                </span>
+              </div>
+            </div>
+
+            <div className="mb-6">
+              <label className="mb-2 block text-black">Message</label>
+              <textarea
+                rows={6}
+                placeholder="Type your message"
+                className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary 
+                disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"></textarea>
+            </div>
+
+            <button className="flex w-full justify-center rounded bg-[#3C50E0] p-3 font-medium text-white">
+              Send Message
+            </button>
           </div>
-        </div>
-        <button
-          type="submit"
-          className="absolute bottom-0 right-10 bg-blue-500 text-white px-12 py-3 rounded-lg"
-          onClick={onNext}>
-          Next
-        </button>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };

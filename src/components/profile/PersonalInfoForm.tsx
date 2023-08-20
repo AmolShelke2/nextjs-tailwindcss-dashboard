86486829d86a2a9dfccf4d6ce294ae33185348fd
Amol <shelkeamol897@gmail.com>
2023-08-20 22:26:38 +0530

    [param]: user profile signUp parameters completed

--- src/components/profile/PersonalInfoForm.tsx
+++ src/components/profile/PersonalInfoForm.tsx
@@ -33,7 +33,7 @@ const PersonalInfoForm: React.FC = () => {
   return (
     <div className="flex flex-col gap-9">
       {/* <!-- Contact Form --> */}
-      <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div className="rounded-lg border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
         <div className="border-b border-stroke py-4 px-6 dark:border-strokedark">
           <h3 className="font-medium text-black">Personal Information</h3>
         </div>
@@ -59,34 +59,147 @@ const PersonalInfoForm: React.FC = () => {
               </div>
             </div>
 
-            <div className="mb-4">
+            <div className="mb-4 flex flex-col gap-6 xl:flex-row">
+              <div className="w-full xl:w-1/2">
+                <label className="mb-2 block text-black">
+                  Email <span className="text-meta-1">*</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Enter your email address"
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                />
+              </div>
+
+              <div className="w-full xl:w-1/2">
+                <label className="mb-2 block text-black">
+                  Phone Number <span className="text-meta-1">(optional)</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="Enter your phone number"
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                />
+              </div>
+            </div>
+
+            <div className="mb-4 w-full xl:w-1/2">
               <label className="mb-2 block text-black">
-                Email <span className="text-meta-1">*</span>
+                Password <span className="text-meta-1"></span>
               </label>
               <input
-                type="email"
-                placeholder="Enter your email address"
+                type="password"
+                placeholder="Password or otp"
                 className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
               />
             </div>
 
             <div className="mb-4">
-              <label className="mb-2 block text-black">Subject</label>
-              <input
-                type="text"
-                placeholder="Select subject"
-                className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-              />
+              <label className="mb-2 block text-black">Studying in</label>
+              <div className="relative z-20 bg-transparent">
+                <select className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary">
+                  <option value="">Currently studying in</option>
+                  <option value="">UG</option>
+                  <option value="">PG</option>
+                  <option value="">8-10</option>
+                  <option value="">11</option>
+                  <option value="">12</option>
+                </select>
+                <span className="absolute top-1/2 right-4 z-30 -translate-y-1/2">
+                  <svg
+                    className="fill-current"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <g opacity="0.8">
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                        fill=""></path>
+                    </g>
+                  </svg>
+                </span>
+              </div>
             </div>
 
             <div className="mb-4">
-              <label className="mb-2 block text-black">Subject</label>
+              <label className="mb-2 block text-black">
+                Educational Interest
+              </label>
               <div className="relative z-20 bg-transparent dark:bg-form-input">
                 <select className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary">
-                  <option value="">Type your subject</option>
-                  <option value="">USA</option>
-                  <option value="">UK</option>
-                  <option value="">Canada</option>
+                  <option value="">Type your education interest</option>
+                  <option value="">Engineering & arch</option>
+                  <option value="">Managment (MBA,BBA)</option>
+                  <option value="">Medical (MBBS, M.S,Dental, )</option>
+                  <option value="">Law(LLB,LLM)</option>
+                  <option value="">Animation & Design (B.Des, M.DES)</option>
+                  <option value="">
+                    Media, Mass communcation and journalism ( B JMC, BMC)
+                  </option>
+                  <option value="">Arts humaties and social sciences</option>
+                  <option value="">Sciences ( B.sc,Msc)</option>
+                  <option value="">Agriculture</option>
+                  <option value="">Compouter Application And IT</option>
+                  <option value="">Pharmacy ( M pharm , B Pharm )</option>
+                  <option value="">Education ( B.ED, MED)</option>
+                  <option value="">Hospality and Tourism ( BHM, MHM)</option>
+                </select>
+                <span className="absolute top-1/2 right-4 z-30 -translate-y-1/2">
+                  <svg
+                    className="fill-current"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <g opacity="0.8">
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                        fill=""></path>
+                    </g>
+                  </svg>
+                </span>
+              </div>
+            </div>
+
+            <div className="mb-4 flex flex-col gap-6 xl:flex-row">
+              <div className="w-full xl:w-1/2">
+                <label className="mb-2 block text-black">Current city</label>
+                <input
+                  type="text"
+                  placeholder="Enter current city"
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                />
+              </div>
+
+              <div className="w-full xl:w-1/2">
+                <label className="mb-2 block text-black">
+                  Interested course
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Interested course"
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                />
+              </div>
+            </div>
+
+            <div className="mb-4">
+              <label className="mb-2 block text-black">
+                Future study location
+              </label>
+              <div className="relative z-20 bg-transparent dark:bg-form-input">
+                <select className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary">
+                  <option value="">States</option>
+                  <option value="">Karnataka</option>
+                  <option value="">Maharastra</option>
+                  <option value="">Andhra Pradesh</option>
                 </select>
                 <span className="absolute top-1/2 right-4 z-30 -translate-y-1/2">
                   <svg
